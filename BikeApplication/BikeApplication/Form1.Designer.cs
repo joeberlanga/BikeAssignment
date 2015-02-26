d729de0fa812d0dc1d30d31df24d8ab36ad35fcf
Joe Berlanga <J.Berlanga6764@student.leedsbeckett.ac.uk>
2015-02-26 13:18:03 +0000

    Parsed data into array
    Parsed all useful data into a single array. Next step is to split the
    values into arrays for specific information because at the minute all
    values for all different categories are in one array. Then populate
    tables with data.

---
+++ BikeApplication/BikeApplication/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace BikeApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtData = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(89, 79);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(496, 413);
+            this.txtData.TabIndex = 0;
+            this.txtData.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 588);
+            this.Controls.Add(this.txtData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtData;
+    }
+}
+
---
+++ BikeApplication/BikeApplication/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace BikeApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getData();
+            displayText();
+        }
+        private string[] data;
+        public void getData()
+        {
+            string path = @"C:\Users\Joe\Documents\UNI\software\BikeApplication\ASDBExampleCycleComputerData.hrm";
+            string line;
+            StreamReader sr = new StreamReader(path);
+
+            while ((line = sr.ReadLine()) != null)
+            {
+          
+
+                if(line.Contains("[HRData]"))
+                {
+                    data = sr.ReadToEnd().Split('\t');
+                    break;
+
+                }                  
+
+            }
+
+
+        }
+        public void displayText()
+        {
+            foreach (string myString in data)
+            {
+                txtData.Text = data[1];
+            }
+
+
+        }
+      
+    }
+}
